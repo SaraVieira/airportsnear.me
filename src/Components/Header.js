fb7c016700352df907f77563c90e4232cb1a3ea7
Sara Vieira <hey@iamsaravieira.com>
2019-03-17 21:17:45 +0100

    hmm

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Autocomplete from 'react-google-autocomplete'
 import { H1 } from '../Elements'
 import header from '../assets/header.svg'
@@ -16,34 +16,28 @@ const AutocompleteStyles = {
   paddingLeft: '0'
 }
 
-const TryComplete = ({ onSelect }) => {
-  let component = null
-  import('react-google-autocomplete').then(({ default: Autocomplete }) => {
-    console.log(Autocomplete)
-    component = (
-      <Autocomplete
-        style={AutocompleteStyles}
-        placeholder="Set Your Location"
-        onPlaceSelected={onSelect}
-      />
-    )
-  })
-
-  return <div />
-}
-
 export default ({ onSelect }) => {
+  const [AutocompleteComponent, setAutoComplete] = useState()
+
+  const TryComplete = ({ onSelect }) => {
+    import('react-google-autocomplete').then(({ default: Autocomplete }) => {
+      setAutoComplete(Autocomplete)
+    })
+  }
   return (
     <>
       <H1> Your closest airport</H1>
-      <TryComplete
-        onSelect={place =>
+      <AutocompleteComponent
+        style={AutocompleteStyles}
+        placeholder="Set Your Location"
+        onPlaceSelected={place =>
           onSelect({
             latitude: place.geometry.location.lat(),
             longitude: place.geometry.location.lng()
           })
         }
       />
+      } />
       <img src={header} width="100%" alt="Landscape of city" />
     </>
   )
